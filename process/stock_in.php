3229591f04905ea26f8706eb004ff9acfefc2aa1
mhee2555 <sarayuth.mhee@gmail.com>
2019-07-01 15:42:28 +0700

    eee

--- process/stock_in.php
+++ process/stock_in.php
@@ -43,7 +43,7 @@ function getDepartment($conn, $DATA)
   $Hotp = $DATA["Hotp"];
   $Sql = "SELECT department.DepCode,department.DepName
 		  FROM department
-		  WHERE department.HptCode = $Hotp
+		  WHERE department.HptCode = '$Hotp'
       AND department.IsDefault = 1
 		  AND department.IsStatus = 0";
   $meQuery = mysqli_query($conn, $Sql);
@@ -82,13 +82,13 @@ function CreateDocument($conn, $DATA)
   //	 $Sql = "INSERT INTO log ( log ) VALUES ('userid : $userid')";
   //     mysqli_query($conn,$Sql);
 
-  $Sql = "SELECT CONCAT('SI',lpad($hotpCode, 3, 0),'/',SUBSTRING(YEAR(DATE(NOW())),3,4),LPAD(MONTH(DATE(NOW())),2,0),'-',
+  $Sql = "SELECT CONCAT('SI',lpad('$hotpCode', 3, 0),'/',SUBSTRING(YEAR(DATE(NOW())),3,4),LPAD(MONTH(DATE(NOW())),2,0),'-',
 LPAD( (COALESCE(MAX(CONVERT(SUBSTRING(DocNo,12,5),UNSIGNED INTEGER)),0)+1) ,5,0)) AS DocNo,DATE(NOW()) AS DocDate,
 CURRENT_TIME() AS RecNow
 FROM stock_in
 INNER JOIN department on stock_in.DepCode = department.DepCode
-WHERE DocNo Like CONCAT('SI',lpad($hotpCode, 3, 0),'/',SUBSTRING(YEAR(DATE(NOW())),3,4),LPAD(MONTH(DATE(NOW())),2,0),'%')
-AND department.HptCode = $hotpCode
+WHERE DocNo Like CONCAT('SI',lpad('$hotpCode', 3, 0),'/',SUBSTRING(YEAR(DATE(NOW())),3,4),LPAD(MONTH(DATE(NOW())),2,0),'%')
+AND department.HptCode = '$hotpCode'
 ORDER BY DocNo DESC LIMIT 1";
 
   $meQuery = mysqli_query($conn, $Sql);
@@ -550,7 +550,7 @@ function getImport($conn, $DATA)
     }
 	$DepCode = 1;
 	$Sql = "SELECT DepCode FROM department
-	WHERE department.HptCode = $Hotp AND department.IsDefault = 1
+	WHERE department.HptCode = '$Hotp' AND department.IsDefault = 1
 	ORDER BY DepCode ASC LIMIT 1";
 	$meQuery = mysqli_query($conn, $Sql);
 	while ($Result = mysqli_fetch_assoc($meQuery)) {
